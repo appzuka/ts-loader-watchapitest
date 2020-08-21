ec6fdbc4fc79cc94b351a43d6a47decab4194bf8
Nick Excell <nick@appzuka.com>
2020-08-21 16:56:18 +0100

    Update generate script

--- generate.ts
+++ generate.ts
@@ -12,10 +12,10 @@ const busyLines = `for(let i=0;i<1000;i++) {
   }
 };`;
 
-const busyWork = 2;  // The number of times to include busyLines in each file
+const busyWork = 0;  // The number of times to include busyLines in each file
 
 const iterate = (i, f) => {
-  return Array(i).fill(0).map((v,i) => f(i)).join('\n')
+  return Array(i).fill(0).map((v,j) => f(j)).join('\n')
 }
 
 let filesGenerated = 0;
@@ -28,25 +28,27 @@ const writeIndexFile = (counters, level) => {
   if (rest.length > 0) {
     fs.writeFileSync(`${baseDir}/generated_${rest.join('_')}.tsx`, 
     `import React from 'react';
-    ${iterate(last, (i) => `import { Generated_${rest.join('_')}_${i} } from './generated_${rest.join('_')}_${i}';`)}
+${iterate(last, (i) => `import { Generated_${rest.join('_')}_${i} } from './generated_${rest.join('_')}_${i}';`)}
     
-    export function Generated_${rest.join('_')} () {
-    <div>
-    ${iterate(last, (i) => `    <Generated_${rest.join('_')}_${i} />`)}
-    </div>
-    }
-    `);
+export function Generated_${rest.join('_')} () {
+${iterate(busyWork, () => busyLines)}
+<div>
+<h2>Generated component ${rest.join('_')}</h2>
+${iterate(last, (i) => `    <Generated_${rest.join('_')}_${i} />`)}
+</div>
+}
+`);
   } else {
     fs.writeFileSync(`${baseDir}/generatedindex.tsx`, 
-    `import React from 'react';
-    ${iterate(last, (i) => `import { Generated_${i} } from './generated_${i}';`)}
+`import React from 'react';
+${iterate(last, (i) => `import { Generated_${i} } from './generated_${i}';`)}
     
-    export function GeneratedContent () {
-    <div>
-    ${iterate(last, (i) => `    <Generated_${i} />`)}
-    </div>
-    }
-    `);
+export function GeneratedContent () {
+<div>
+${iterate(last, (i) => `    <Generated_${i} />`)}
+</div>
+}
+`);
   }
 
 }
@@ -57,18 +59,7 @@ const depths = [4,4,4,4,4];
 const counters = new Array(depths.length).fill(0);
 
 const nestedLoopOperation = (counters, depths, level) => {
-    if (level === counters.depths) {
-      filesGenerated += 1;
-      fs.writeFileSync(`${baseDir}/generated_${counters.join('_')}.tsx`, 
-      `import React from 'react';
-      
-      ${iterate(busyWork, () => busyLines)}
-      
-      export function Generated_${counters.join('_')}() {
-        return <h2>Generated component ${counters.join('_')}</h2>;
-      }
-      `)
-    } else {
+    if (level !== counters.depths) {
         for (counters[level] = 0; counters[level] < depths[level]; counters[level]++) {
             nestedLoopOperation(counters, depths, level + 1);
         }
